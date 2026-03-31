11be723ba1687a1c6c60a53c507ee27405c46267
Kile Asmussen <kile.asmussen@gmail.com>
2026-04-01 01:47:15 +0200

    .

--- tweaks/ores.lua
+++ tweaks/ores.lua
@@ -20,56 +20,18 @@ function tweaks.ores.data_final_fixes()
 
         if resource.category == nil or resource.category == 'basic-solid' then
 
-            -- Make the ores infinite
-            -- resource.infinite = true
+            resource.infinite = true
             
-            -- resource.normal = 100
-            -- resource.minimum = 1
+            resource.normal = 100
+            resource.minimum = 100
              
-            -- resource.minable.mining_time = 1
+            resource.minable.mining_time = 1
             
-            -- resource.stage_counts = { 600, 400, 300, 150, 100, 50, 25, 17 }
-            
-            local richness_multiplier_setting = "control-setting:" .. resource.name .. ":richness"
-            
-            resource.autoplace.richness_expression = "1000000"
+            resource.stage_counts = { 600, 400, 300, 150, 100, 50, 25, 17 }
 
-            -- "(var('control:iron-ore:size') > 0) * (1*var('control:iron-ore:richness')*(var('default-iron-ore-patches'))*max((1000+distance)/2600,1))"
-             -- * var('" .. richness_multiplier_setting .. "')
-
-            --[[
-            (var('control:iron-ore:size') > 0)
-            * (
-                var('control:iron-ore:richness')
-                * var('default-iron-ore-patches')
-                * max(
-                    (1000+distance)/2600,
-                    1
-                )
-            )
-            ]]
+            local richness_multiplier_setting = "control:" .. resource.name .. ":richness"
+            
+            resource.autoplace.richness_expression = "100 * var('" .. richness_multiplier_setting .. "')"
         end
     end 
 end
-
---[[
-function tweaks.ores.richness_expression(varname)
-    return {
-        type = 'function-application',
-        function_name = 'multiply',
-        source_location = { filename = "", line_number = 0 },
-        arguments = {
-            {
-                literal_value = 100,
-                source_location = { filename = "", line_number = 0 },
-                type = 'literal-number'
-            },
-            {
-                variable_name = varname,
-                source_location = { filename = "", line_number = 0 },
-                type = 'variable'
-            }
-        }
-    }    
-end
-]]
