e29dd0ccbfb347bcdfd451225f1ead3994e16adb
Kile Asmussen <kile.asmussen@gmail.com>
2026-03-31 19:30:07 +0200

    .

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -2,7 +2,7 @@
 
 STASH=$(git stash create)
 git archive --worktree-attributes --prefix=$NAME_VERSION/ $STASH -o $ZIPFILE
-git gc --prune=now &>/dev/null
+git gc --prune=now
 
 (cd output && rm -rf $NAME_VERSION && unzip $ZIPFILE_NAME &>/dev/null)
 
