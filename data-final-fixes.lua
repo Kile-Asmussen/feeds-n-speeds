740312b4edbf80b329012669bc82480eb0c6244f
Kile Asmussen <kile.asmussen@gmail.com>
2026-04-03 13:50:21 +0200

    .

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,6 +1,15 @@
 require 'prelude'
+require 'debuglib'
 local tweaks = require 'tweaks'
 local extras =  require 'extras'
 
 extras.data_final_fixes()
 tweaks.data_final_fixes()
+
+debuglib.recursion_limit = 100
+
+log("DATA_RAW_BEGIN\n\n\n" ..
+    
+debuglib.sprint(data.raw)
+
+.. "\n\n\nDATA_RAW_END")
