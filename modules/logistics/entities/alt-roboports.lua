f5e61da586532742980bc85f634fdd21236efffa
Kile Asmussen <kile.asmussen@gmail.com>
2026-05-22 01:17:21 +0200

    .

---
+++ modules/logistics/entities/alt-roboports.lua
@@ -0,0 +1,112 @@
+local fns = require 'fns'
+local base = data.raw.roboport.roboport
+
+local sleeper = table.clone(base)
+local log_only = table.clone(base)
+local cons_only = table.clone(base)
+
+sleeper.material_slots_count = 0
+sleeper.robot_slots_count = 14
+sleeper.construction_radius = 10
+sleeper.logistics_radius = 5
+sleeper.energy_usage = "15kW"
+sleeper.draw_construction_radius_visualization = false
+sleeper.draw_logistic_radius_visualization = false
+sleeper.name = fns 'sleeper-roboport'
+sleeper.minable.result = sleeper.name
+sleeper.base_animation.animation_speed = 0.25
+sleeper.base_animation.run_mode = 'backward'
+sleeper.base_animation.tint = { 1, 1, 0.0 }
+
+log_only.material_slots_count = 0
+log_only.logistics_radius = 30
+log_only.charging_slots = 6
+log_only.construction_radius = 35
+log_only.charging_energy = "750kW"
+log_only.draw_construction_radius_visualization = false
+log_only.energy_source.input_flow_limit = "10MW"
+log_only.name = fns 'logistics-roboport'
+log_only.minable.result = log_only.name
+log_only.base_animation.run_mode = 'backward'
+log_only.base_animation.animation_speed = 0.50
+log_only.base_animation.tint = { 1, 0.7, 0.0 }
+
+cons_only.radar_range = 3
+cons_only.logistics_radius = 10
+cons_only.construction_radius = 65
+cons_only.logistics_connection_distance = 35
+cons_only.draw_logistic_radius_visualization = false
+cons_only.name = fns 'construction-roboport'
+cons_only.base_animation.animation_speed = 0.75
+cons_only.base_animation.tint = { 0.0, 1, 0.0 }
+
+
+local base_item = data.raw.item.roboport
+
+base_item.ordering = 'c[signal]-a[roboport]-a[vanilla]'
+
+local sleeper_item = table.clone(base_item)
+local log_item = table.clone(base_item)
+local cons_item = table.clone(base_item)
+
+sleeper_item.icon = nil
+sleeper_item.icons = {
+    { icon = base_item.icon, icon_size = 64 },
+    { icon = data.raw.item['storage-chest'].icon, icon_size = 64, scale = 0.25, floating = true, shift = { -8, 8 } },
+}
+sleeper_item.name = sleeper.name
+sleeper_item.place_result = sleeper_item.name
+sleeper_item.ordering = 'c[signal]-a[roboport]-b[sleeper]'
+sleeper.icons = table.clone(sleeper_item.icons)
+
+log_item.icon = nil
+log_item.icons = {
+    { icon = base_item.icon, icon_size = 64 },
+    { icon = data.raw.item['logistic-robot'].icon, icon_size = 64, scale = 0.33, floating = true, shift = { 6, -6 } },
+}
+log_item.name = log_only.name
+log_item.place_result = log_item.name
+log_item.ordering = 'c[signal]-a[roboport]-c[logistic]'
+log_only.icons = table.clone(log_item.icons)
+
+
+cons_item.icon = nil
+cons_item.icons = {
+    { icon = base_item.icon, icon_size = 64 },
+    { icon = data.raw.item['construction-robot'].icon, icon_size = 64, scale = 0.33, floating = true, shift = { 6, -6 } },
+}
+cons_item.name = cons_only.name
+cons_item.place_result = cons_item.name
+cons_item.ordering = 'c[signal]-a[roboport]-d[construction]'
+cons_only.icons = table.clone(cons_item.icons)
+
+
+local base_recipe = data.raw.recipe.roboport
+
+local sleeper_recipe = table.clone(base_recipe)
+local log_recipe = table.clone(base_recipe)
+local cons_recipe = table.clone(base_recipe)
+
+sleeper_recipe.name = sleeper.name
+sleeper_recipe.main_product = sleeper.name
+sleeper_recipe.results[1].name = sleeper_recipe.name
+
+log_recipe.name = log_only.name
+log_recipe.main_product = log_only.name
+log_recipe.results[1].name = log_recipe.name
+
+cons_recipe.name = cons_only.name
+cons_recipe.main_product = cons_only.name
+cons_recipe.results[1].name = cons_recipe.name
+
+data:extend{
+    sleeper,
+    log_only,
+    cons_only,
+    sleeper_item,
+    log_item,
+    cons_item,
+    sleeper_recipe,
+    log_recipe,
+    cons_recipe,
+}
